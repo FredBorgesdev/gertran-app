77a6748c3293ab317c6f923016ba55772b9aa2ad
Vitor Freitas <vitorldfreitas@gmail.com>
2022-09-03 19:09:40 -0300

    authenticate dashboard/home

---
+++ src/app/authentication/auth.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthenticationService } from './authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) { }
+
+  canActivate() {
+    if (this.authService.isAuthenticated()) return true;
+
+    this.router.navigate(['/authentication/login']);
+  }
+}
--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '../authentication/auth.guard';
 import { DashboardComponent } from './dashboard.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: DashboardComponent,
+    canActivate: [AuthGuard],
     data: {
       title: 'Dashboard ',
       headerDisplay: "none"
