a64bc0e8fb70f23552464e34e8a5022ec7bdd761
Vitor Freitas <vitorldfreitas@gmail.com>
2022-09-10 15:48:05 -0300

    sentry environment

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ Sentry.init({
       routingInstrumentation: Sentry.routingInstrumentation,
     }),
   ],
-
+  environment: environment.production ? 'production' : 'development',
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
