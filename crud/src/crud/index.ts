cd46718beb0604c43260563b637ba868446e07df
Vitor Freitas <vitorldfreitas@gmail.com>
2022-09-10 14:18:52 -0300

    crud schematics

--- crud/src/crud/index.ts
+++ crud/src/crud/index.ts
@@ -1,17 +1,31 @@
-import { mergeWith, Rule, SchematicContext, Tree, url, apply, template, strings } from '@angular-devkit/schematics';
+import { mergeWith, Rule, SchematicContext, Tree, url, apply, template, strings, SchematicsException, move } from '@angular-devkit/schematics';
+import { parseName } from "@schematics/angular/utility/parse-name";
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function crud(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    console.log('Running schematics with following options', _options);
+    const workspaceAsBuffer = tree.read('angular.json');
+    if (!workspaceAsBuffer) {
+      throw new SchematicsException('We are not inside of Angular CLI workspace');
+    }
+
+    const workspace = JSON.parse(workspaceAsBuffer.toString());
+    const projectName = _options.project || workspace.defaultProject;
+    const project = workspace.projects[projectName];
+    const sourceRoot = project.sourceRoot;
+    const projectType = project.projectType;
+    const type = projectType === 'application' ? 'app' : 'lib';
+    const path = `${sourceRoot}/${type}`;
+    const parsed = parseName(path, _options.name);
+    _options.name = parsed.name;
 
     const sourceTpl = url('./files');
-    const sourceTplParametrized = apply(sourceTpl, [template({
-      ..._options,
-      ...strings
-    })]);
+    const sourceTplParametrized = apply(sourceTpl, [
+      template({ ..._options, ...strings }),
+      move(parsed.path)
+    ]);
 
     return mergeWith(sourceTplParametrized)(tree, _context);
   };
