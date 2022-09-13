9288c3d433a6ba48902fcbd41940b9bdbd287244
Vitor Freitas <vitorldfreitas@gmail.com>
2022-09-13 19:53:18 -0300

    update crud generator

--- crud/src/crud/files/__name@dasherize__/__name@dasherize__-list/__name@dasherize__-list.component.ts
+++ crud/src/crud/files/__name@dasherize__/__name@dasherize__-list/__name@dasherize__-list.component.ts
@@ -3,16 +3,14 @@ import { Router } from '@angular/router';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { NzModalService } from 'ng-zorro-antd/modal';
 import { <%= classify(name) %>Service, <%= classify(name) %> } from '../<%= dasherize(name) %>.service';
+import { BaseCrudListComponent } from '../../base-crud/base-crud-list/base-crud-list.component';
 
 @Component({
   selector: 'app-<%= dasherize(name) %>-list',
   templateUrl: './<%= dasherize(name) %>-list.component.html',
   styleUrls: ['./<%= dasherize(name) %>-list.component.css'],
 })
-export class <%= classify(name) %>ListComponent implements OnInit {
-  isLoading = false
-  <%= camelize(name) %>List: <%= classify(name) %>[] = []
-
+export class <%= classify(name) %>ListComponent extends BaseCrudListComponent<<%= classify(name) %>> {
   <%= camelize(name) %>Columns = [
     { title: 'Id' },
     { title: 'Nome' },
@@ -20,48 +18,25 @@ export class <%= classify(name) %>ListComponent implements OnInit {
   ]
 
   constructor(
-    private router: Router,
-    private service: <%= classify(name) %>Service,
-    private message: NzMessageService,
-    private modal: NzModalService,
-  ) { }
-
-  ngOnInit(): void {
-    this.isLoading = true
-    this.service.getAll().subscribe((data) => {
-      this.<%= camelize(name) %>List = data
-      this.isLoading = false
-    })
+    router: Router,
+    service: <%= classify(name) %>Service,
+    message: NzMessageService,
+    modal: NzModalService,
+  ) {
+    super(
+      '<%= dasherize(name) %>',
+      router,
+      service,
+      message,
+      modal,
+    );
   }
 
-  create(){
-    this.router.navigate(['/<%= dasherize(name) %>', '<%= dasherize(name) %>-create'])
-  }
-
-  edit(item: <%= classify(name) %>){
-    this.router.navigate(['/<%= dasherize(name) %>', '<%= dasherize(name) %>-edit', item.id])
-  }
-
-  delete(item: <%= classify(name) %>) {
-    this.modal.confirm({
-      nzTitle: 'Deseja realmente excluir?',
-      nzContent: 'Essa ação não poderá ser desfeita',
-      nzOkText: 'Sim',
-      nzOnOk: () => this.handleDelete(item.id),
-    })
-  }
-
-  handleDelete(id: string) {
-    this.isLoading = true
-    this.service.delete(id).subscribe(() => {
-      this.<%= camelize(name) %>List = this.<%= camelize(name) %>List.filter(
-        (item) => item.id !== id
-      )
-      this.message.success('<%= ptName %> excluído com sucesso')
-      this.isLoading = false
-    }, () => {
-      this.message.error('Erro ao excluir <%= ptName %>')
-      this.isLoading = false
-    })
+  loadResources(url?: string): void {
+    this.isLoading = true;
+    this.service.getAll({ url }).subscribe((data) => {
+      this.resources = data;
+      this.isLoading = false;
+    });
   }
 }
