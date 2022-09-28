bbe09aed4e262de7fdca185f559ed69f38afc7f9
Vitor Freitas <vitorldfreitas@gmail.com>
2022-09-27 21:22:16 -0300

    add base crud form to schematics

--- crud/src/crud/files/__name@dasherize__/__name@dasherize__-form/__name@dasherize__-form.component.ts
+++ crud/src/crud/files/__name@dasherize__/__name@dasherize__-form/__name@dasherize__-form.component.ts
@@ -3,79 +3,36 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { <%= classify(name) %>Service, <%= classify(name) %> } from '../<%= dasherize(name) %>.service';
+import { BaseCrudFormComponent } from '../../base-crud/base-crud-form/base-crud-form.component';
 
 @Component({
   selector: 'app-<%= dasherize(name) %>-form',
   templateUrl: './<%= dasherize(name) %>-form.component.html',
   styleUrls: ['./<%= dasherize(name) %>-form.component.css'],
 })
-export class <%= classify(name) %>FormComponent implements OnInit {
-  isLoading = false
-  <%= camelize(name) %>: <%= classify(name) %> = null
-
-  validateForm: FormGroup
-
+export class <%= classify(name) %>FormComponent extends BaseCrudFormComponent<<%= classify(name) %>> {
   constructor(
     private router: Router,
     private formBuilder: FormBuilder,
-    private activatedRoute: ActivatedRoute,
-    private service: <%= classify(name) %>Service,
-    private message: NzMessageService,
-  ) { }
+    activatedRoute: ActivatedRoute,
+    service: <%= classify(name) %>Service,
+    message: NzMessageService,
+  ) {
+    super(
+      service,
+      message,
+      activatedRoute,
+    );
+  }
 
-  ngOnInit(): void {
+  loadFormBuilder(): void {
     this.validateForm = this.formBuilder.group({
-    })
-    this.load<%= classify(name) %>()
+      name: [null, [Validators.required]],
+      peripheralType: [null, [Validators.required]],
+    });
   }
 
-  load<%= classify(name) %>() {
-    if (this.activatedRoute.snapshot.paramMap.has('id')) {
-      this.isLoading = true
-      const id = this.activatedRoute.snapshot.paramMap.get('id')
-      this.service.get(id).subscribe(<%= camelize(name) %> => {
-        this.<%= camelize(name) %> = <%= camelize(name) %>
-
-        this.validateForm.patchValue({
-        })
-
-        this.isLoading = false
-      })
-    }
-  }
-
-  list() {
-    this.router.navigate(['/<%= dasherize(name) %>/<%= dasherize(name) %>-list'])
-  }
-
-  save() {
-    if (!this.validateForm.valid) {
-      Object.values(this.validateForm.controls).forEach(control => {
-        control.markAsDirty();
-        control.updateValueAndValidity({ onlySelf: true });
-      });
-    }
-
-    this.isLoading = true
-    if (this.<%= camelize(name) %>?.id) {
-      this.service.update(
-        this.<%= camelize(name) %>.id,
-        this.validateForm.value
-      ).subscribe(() => this.handleSuccess(), () => this.handleError())
-    } else {
-      this.service.save(this.validateForm.value)
-        .subscribe(() => this.handleSuccess(), () => this.handleError())
-    }
-  }
-
-  private handleSuccess() {
-    this.message.success('Registro salvo com sucesso')
-    this.list()
-    this.isLoading = false
-  }
-
-  private handleError() {
-    this.message.error('Ocorreu um erro ao salvar o registro')
-    this.isLoading = false
+  list(): void {
+    this.router.navigate(['/<%= dasherize(name) %>/<%= dasherize(name) %>-list']);
   }
 }
