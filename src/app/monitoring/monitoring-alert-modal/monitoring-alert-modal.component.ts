211ba3ee32479dcfdfc5bd016508f2193b79d1e7
Vitor Freitas <vitorldfreitas@gmail.com>
2022-10-21 23:27:23 -0300

    alert modal

---
+++ src/app/monitoring/monitoring-alert-modal/monitoring-alert-modal.component.ts
@@ -0,0 +1,63 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {NzModalService} from 'ng-zorro-antd/modal';
+
+@Component({
+  selector: 'app-monitoring-alert-modal',
+  templateUrl: './monitoring-alert-modal.component.html',
+  styleUrls: ['./monitoring-alert-modal.component.css']
+})
+export class MonitoringAlertModalComponent implements OnInit {
+  @Input() urgent = false;
+
+  isUrgentModalOpen = false;
+  isLoading = false;
+  urgentMessage: string = '';
+
+  list = [
+    {
+      id: 1,
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      date: '2020-01-01 00:00:00',
+    },
+    {
+      id: 2,
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      date: '2020-01-01 00:00:00',
+    }
+  ];
+
+  constructor(
+    private modal: NzModalService,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  markAsRead(item: any): void {
+    if (this.urgent) {
+      this.isUrgentModalOpen = true;
+      return;
+    }
+
+    console.log(item);
+  }
+
+  resolveUrgency(): void {
+    this.modal.confirm({
+      nzTitle: 'Deseja realmente resolver a urgência?',
+      nzContent: 'Ao resolver a urgência, o alerta será marcado como lido e não será mais exibido.',
+      nzOkText: 'Sim',
+      nzOkType: 'primary',
+      nzOnOk: () => {
+        this.isLoading = true;
+        setTimeout(() => {
+          this.isLoading = false;
+          this.isUrgentModalOpen = false;
+        }, 1000);
+      },
+      nzCancelText: 'Não',
+    });
+  }
+}
