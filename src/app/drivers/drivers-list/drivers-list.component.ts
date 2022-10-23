e0d566aad0f26cc4e438095bb03f7744e169f981
Vitor Freitas <vitorldfreitas@gmail.com>
2022-10-23 16:21:15 -0300

    extend driver from base list classg

--- src/app/drivers/drivers-list/drivers-list.component.ts
+++ src/app/drivers/drivers-list/drivers-list.component.ts
@@ -1,25 +1,20 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { NzModalService } from 'ng-zorro-antd/modal';
-import { NzTableQueryParams } from 'ng-zorro-antd/table';
-import { GetAllResponse, getCurrentPage } from 'src/app/shared/services/api.service';
 
 import { TableService } from '../../shared/services/table.service';
 import { Driver, DriversService } from '../drivers.service';
+import { BaseCrudListComponent } from '../../base-crud/base-crud-list/base-crud-list.component';
 
 @Component({
   selector: 'app-drivers-list',
   templateUrl: './drivers-list.component.html',
   styleUrls: [ './drivers-list.component.css' ]
 })
-export class DriversListComponent implements OnInit {
+export class DriversListComponent extends BaseCrudListComponent<Driver> {
 
   isLoading = false;
-  displayData: GetAllResponse<Driver> = null;
   searchInput: string;
 
   driverColumn = [
@@ -39,71 +34,37 @@ export class DriversListComponent implements OnInit {
   ];
 
   constructor(
-    private router: Router,
-    private tableService: TableService,
-    private driversService: DriversService,
-    private message: NzMessageService,
-    private modal: NzModalService,
-  ) {}
-
-  ngOnInit(): void {
-    this.loadDrivers();
+    router: Router,
+    tableService: TableService,
+    driversService: DriversService,
+    message: NzMessageService,
+    modal: NzModalService,
+  ) {
+    super('drivers', router, driversService, message, modal);
   }
 
-  loadDrivers(url?: string) {
-    this.isLoading = true;
-    this.driversService.getAll({ url }).subscribe(
-      data => {
-        this.displayData = data;
-        this.isLoading = false;
-      },
-      () => {
-        this.message.error('Falha ao carregar motoristas');
-        this.isLoading = false;
-      }
-    );
-  }
+  search(): void {}
 
-
-  search() {
-    this.displayData.results = this.tableService.search(
-      this.searchInput,
-      this.displayData.results
-    );
-  }
-
-  create() {
+  create(): void {
     this.router.navigate(['/drivers/driver-create']);
   }
 
-  edit(item: Driver) {
+  edit(item: Driver): void {
     this.router.navigate(['/drivers/driver-edit', item.id]);
   }
 
-  delete(item: Driver) {
+  delete(item: Driver): void {
     this.modal.confirm({
       nzTitle: 'Você tem certeza que deseja excluir este motorista?',
       nzOnOk: () => {
-        this.driversService.delete(item.id).subscribe(
+        this.service.delete(item.id).subscribe(
           () => {
             this.message.success('Motorista excluído com sucesso');
-            this.loadDrivers();
+            this.loadResources();
           },
           () => this.message.error('Falha ao excluir motorista')
         );
       }
     });
-  }
-
-  get page() {
-    return getCurrentPage(this.displayData);
-  }
-
-  handleQueryParamsChange(params: NzTableQueryParams): void {
-    if (params.pageIndex < this.page) {
-      this.loadDrivers(this.displayData.previous);
-    } else if (params.pageIndex > this.page) {
-      this.loadDrivers(this.displayData.next);
-    }
   }
 }
