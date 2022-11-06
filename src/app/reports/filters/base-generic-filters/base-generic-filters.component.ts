be113e430955636120f43fd8d094cc8865614ea7
Vitor Freitas <vitorldfreitas@gmail.com>
2022-11-06 15:52:40 -0300

    add export to xlsx to base-generic-filter

--- src/app/reports/filters/base-generic-filters/base-generic-filters.component.ts
+++ src/app/reports/filters/base-generic-filters/base-generic-filters.component.ts
@@ -1,5 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import {XlsxExporterService} from '../../../shared/services/xlsx-exporter.service';
+import {NzMessageService} from 'ng-zorro-antd/message';
 
 export interface BaseGenericFilter {
   plate: string;
@@ -16,10 +18,16 @@ export interface BaseGenericFilter {
 export class BaseGenericFiltersComponent implements OnInit {
   @Output() generateReport = new EventEmitter<BaseGenericFilter>();
   @Input() hideButtons = false;
+  @Input() rows: any[];
+  @Input() fileName = 'relatorio';
 
   validateForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private xlsxExporterService: XlsxExporterService,
+    private message: NzMessageService,
+  ) { }
 
   ngOnInit(): void {
     this.validateForm = this.formBuilder.group({
@@ -34,5 +42,15 @@ export class BaseGenericFiltersComponent implements OnInit {
     if (this.validateForm.valid) {
       this.generateReport.emit(this.validateForm.value);
     }
+  }
+
+  generateExcel(): void {
+    if (!this.rows || this.rows.length === 0) {
+      this.message.error('Não há dados para exportar');
+      return;
+    }
+    const fileNameWithPlate = `${this.fileName} - ${this.validateForm.value.plate}`;
+
+    this.xlsxExporterService.generate(fileNameWithPlate, this.rows);
   }
 }
