2617dc368b8d4c92a685ac332206d1e84a0e246d
Vitor Freitas <vitorldfreitas@gmail.com>
2022-12-04 21:53:48 -0300

    fix env

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://8f25-2804-2488-5084-7d70-a809-505c-f7cb-538.sa.ngrok.io/v1',
+  apiUrl: 'https://api.gertran.zayit.com.br:443/v1',
   sentryDsn: 'https://ad60a38f5d014bb091362c6415a6a6d1@o124348.ingest.sentry.io/6727350',
   appUrl: 'https://gertran.zayit.com.br',
   mapboxAccessToken: 'pk.eyJ1Ijoidml0b3JsZGZyZWl0YXMiLCJhIjoiY2w4amppY25kMDQ4ODNucWc5Ynh6MTc4biJ9.9T9N2GtMEAwgo88NSwHayA'
