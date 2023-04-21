480337bba0132ac901c6137555fc1ed6e6adc3ed
Vitor Freitas <vitorldfreitas@gmail.com>
2023-04-21 20:31:22 -0300

    add refresh to monitoring request list

--- src/app/monitoring-requests/monitoring-requests-list/monitoring-requests-list.component.ts
+++ src/app/monitoring-requests/monitoring-requests-list/monitoring-requests-list.component.ts
@@ -17,6 +17,7 @@ import {
 import {MonitoringRequestsFilter} from '../monitoring-requests-filter/monitoring-requests-filter.component';
 import {AuthenticationService} from '../../authentication/authentication.service';
 import User from '../../users/user';
+import {Observable, timer} from "rxjs";
 
 @Component({
   selector: 'app-monitoring-requests-list',
@@ -46,6 +47,8 @@ export class MonitoringRequestsListComponent extends BaseCrudListComponent<Monit
 
   refreshAfterClose = new EventEmitter<ModalDestroyResult>();
 
+  monitoringRequestRefresher: Observable<number>;
+
   constructor(
     private directionsService: DirectionsService,
     public authService: AuthenticationService,
@@ -66,7 +69,10 @@ export class MonitoringRequestsListComponent extends BaseCrudListComponent<Monit
   async ngOnInit(): Promise<void> {
     super.ngOnInit();
 
-    await this.loadAllResources();
+    this.monitoringRequestRefresher = timer(0, 2 * 60 * 1000);
+    this.monitoringRequestRefresher.subscribe(async () => {
+      await this.loadAllResources();
+    });
 
     this.refreshAfterClose.subscribe((result) => {
       if (result?.updateList) {
