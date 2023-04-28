6a2ea4d02fb09581fdb72b3026525391b740fbe4
Caique Santana <caique.santana10.cs@gmail.com>
2023-04-28 14:47:42 -0300

    feature: add the ability to non-getran users to create and edit routes

--- src/app/routes/routes-form/routes-form.component.ts
+++ src/app/routes/routes-form/routes-form.component.ts
@@ -1,25 +1,32 @@
-import {Component, OnInit} from '@angular/core';
-import {TransferItem, TransferSearchChange} from 'ng-zorro-antd/transfer';
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
-import {Stop, StopsService} from '../../stops/stops.service';
-import {NzMessageService} from 'ng-zorro-antd/message';
-import {BaseCrudFormComponent} from '../../base-crud/base-crud-form/base-crud-form.component';
-import {Route, RoutesService} from '../routes.service';
-import {ActivatedRoute, Router} from '@angular/router';
-import {FormBuilder, Validators} from '@angular/forms';
-import {SelectableCustomerServiceService} from '../../customers/selectable-customer-service.service';
-import {GetAllResponse, getCurrentPage} from '../../shared/services/api.service';
-import {NzTableQueryParams} from 'ng-zorro-antd/table';
-import {debounceTime} from 'rxjs/operators';
-import {Subject} from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { TransferItem, TransferSearchChange } from 'ng-zorro-antd/transfer';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Stop, StopsService } from '../../stops/stops.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { BaseCrudFormComponent } from '../../base-crud/base-crud-form/base-crud-form.component';
+import { Route, RoutesService } from '../routes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
+import { SelectableCustomerServiceService } from '../../customers/selectable-customer-service.service';
+import {
+  GetAllResponse,
+  getCurrentPage,
+} from '../../shared/services/api.service';
+import { NzTableQueryParams } from 'ng-zorro-antd/table';
+import { debounceTime } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { AuthenticationService } from '../../authentication/authentication.service';
 
 @Component({
   selector: 'app-routes-form',
   templateUrl: './routes-form.component.html',
   styleUrls: ['./routes-form.component.css'],
-  providers: [SelectableCustomerServiceService]
+  providers: [SelectableCustomerServiceService],
 })
-export class RoutesFormComponent extends BaseCrudFormComponent<Route> implements OnInit {
+export class RoutesFormComponent
+  extends BaseCrudFormComponent<Route>
+  implements OnInit
+{
   points: TransferItem[] = [];
   pointsResponse: GetAllResponse<Stop>;
   pointsLoading = false;
@@ -30,30 +37,31 @@ export class RoutesFormComponent extends BaseCrudFormComponent<Route> implements
     private router: Router,
     private formBuilder: FormBuilder,
     public selectableCustomerService: SelectableCustomerServiceService,
+    public authService: AuthenticationService,
     routesService: RoutesService,
     message: NzMessageService,
-    activatedRoute: ActivatedRoute,
+    activatedRoute: ActivatedRoute
   ) {
-    super(
-      routesService,
-      message,
-      activatedRoute,
-    );
+    super(routesService, message, activatedRoute);
   }
 
   async ngOnInit(): Promise<void> {
     super.ngOnInit();
 
-    this.selectableCustomerService.init();
+    if (!this.authService.customerId) {
+      this.selectableCustomerService.init();
+    }
 
-    this.searchPointsSubject.pipe(debounceTime(1000)).subscribe((params: TransferSearchChange) => {
-      this.loadPointsFromCustomer(undefined, {name: params.value});
-    });
+    this.searchPointsSubject
+      .pipe(debounceTime(1000))
+      .subscribe((params: TransferSearchChange) => {
+        this.loadPointsFromCustomer(undefined, { name: params.value });
+      });
   }
 
   loadFormBuilder(): void {
     this.validateForm = this.formBuilder.group({
-      customer: [null, [Validators.required]],
+      customer: [this.authService.customerId || null, [Validators.required]],
       name: [null, [Validators.required]],
       code: [null, [Validators.required]],
       description: [null, [Validators.required]],
@@ -66,15 +74,18 @@ export class RoutesFormComponent extends BaseCrudFormComponent<Route> implements
 
   performResourceChange(): void {
     if (this.points.length === 0) {
-      this.stopsService.getAll({limit: 15}).subscribe((stops) => {
-        this.points = this.mapStopsToTransferItems([
-          ...this.resource.points.map((point) => point.point),
-          ...stops.results
-        ]);
-        this.loadCurrentPoints();
-      }, () => {
-        this.message.error('Não foi possível carregar os pontos');
-      });
+      this.stopsService.getAll({ limit: 15 }).subscribe(
+        (stops) => {
+          this.points = this.mapStopsToTransferItems([
+            ...this.resource.points.map((point) => point.point),
+            ...stops.results,
+          ]);
+          this.loadCurrentPoints();
+        },
+        () => {
+          this.message.error('Não foi possível carregar os pontos');
+        }
+      );
     } else {
       this.loadCurrentPoints();
     }
@@ -95,14 +106,16 @@ export class RoutesFormComponent extends BaseCrudFormComponent<Route> implements
   }
 
   drop(event: CdkDragDrop<string[]>): void {
-    const rightItems = this.points.filter(item => item.direction === 'right');
-    const leftItems = this.points.filter(item => item.direction === 'left');
+    const rightItems = this.points.filter((item) => item.direction === 'right');
+    const leftItems = this.points.filter((item) => item.direction === 'left');
     moveItemInArray(rightItems, event.previousIndex, event.currentIndex);
     this.points = [...leftItems, ...rightItems];
   }
 
   save(): void {
-    const selectedPoints = this.points.filter(item => item.direction === 'right');
+    const selectedPoints = this.points.filter(
+      (item) => item.direction === 'right'
+    );
     const pointsIdAndOrder = selectedPoints.map((item, index) => ({
       id: item.key,
       order: index + 1,
@@ -128,35 +141,54 @@ export class RoutesFormComponent extends BaseCrudFormComponent<Route> implements
   }
 
   get selectedPoints(): TransferItem[] {
-    return this.points.filter(item => item.direction === 'right');
+    return this.points.filter((item) => item.direction === 'right');
   }
 
-  loadPointsFromCustomer(url?: string, filters?: {
-    name?: string;
-  }): void {
+  loadPointsFromCustomer(
+    url?: string,
+    filters?: {
+      name?: string;
+    }
+  ): void {
     this.pointsLoading = true;
-    this.stopsService.getAll({
-      url,
-      limit: 15
-    }, {
-      customer: this.validateForm.get('customer').value,
-      name: filters?.name,
-    }).subscribe((stops) => {
-      this.points = this.mapStopsToTransferItems(stops.results).concat(this.selectedPoints);
-      this.pointsResponse = stops;
-      this.pointsLoading = false;
-    }, () => {
-      this.message.error('Não foi possível carregar os pontos');
-      this.pointsLoading = false;
-    });
+    this.stopsService
+      .getAll(
+        {
+          url,
+          limit: 15,
+        },
+        {
+          customer: this.validateForm.get('customer').value,
+          name: filters?.name,
+        }
+      )
+      .subscribe(
+        (stops) => {
+          this.points = this.mapStopsToTransferItems(stops.results).concat(
+            this.selectedPoints
+          );
+          this.pointsResponse = stops;
+          this.pointsLoading = false;
+        },
+        () => {
+          this.message.error('Não foi possível carregar os pontos');
+          this.pointsLoading = false;
+        }
+      );
   }
 
   handlePointsQueryParamsChange(params: NzTableQueryParams): void {
     if (params.pageIndex < this.pointsPage) {
-      const url = this.replaceOffsetWithPage(this.pointsResponse.previous, params.pageIndex);
+      const url = this.replaceOffsetWithPage(
+        this.pointsResponse.previous,
+        params.pageIndex
+      );
       this.loadPointsFromCustomer(url);
     } else if (params.pageIndex > this.pointsPage) {
-      const url = this.replaceOffsetWithPage(this.pointsResponse.next, params.pageIndex);
+      const url = this.replaceOffsetWithPage(
+        this.pointsResponse.next,
+        params.pageIndex
+      );
       this.loadPointsFromCustomer(url);
     }
   }
@@ -168,7 +200,7 @@ export class RoutesFormComponent extends BaseCrudFormComponent<Route> implements
   replaceOffsetWithPage(url: string, page: number): string {
     const limit = +url.match(/limit=\d+/)[0].split('=')[1];
 
-    return url.replace(/offset=\d+/, `offset=${(limit * page) - limit}`);
+    return url.replace(/offset=\d+/, `offset=${limit * page - limit}`);
   }
 
   searchPointsByName(params: TransferSearchChange): void {
