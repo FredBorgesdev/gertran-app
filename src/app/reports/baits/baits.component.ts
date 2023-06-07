90ae16c93b1a4bade4e6d6d95ddf2494f248b1d4
Vitor Freitas <vitorldfreitas@gmail.com>
2023-06-06 21:14:37 -0300

    fix bait report

--- src/app/reports/baits/baits.component.ts
+++ src/app/reports/baits/baits.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CustomerFilter} from '../filters/base-customer-filter/base-customer-filter.component';
-import {ReportsService} from '../reports.service';
+import {MonitoringRequestBait, ReportsService} from '../reports.service';
 
 @Component({
   selector: 'app-baits',
@@ -9,18 +9,20 @@ import {ReportsService} from '../reports.service';
 })
 export class BaitsComponent implements OnInit {
   isLoading = false;
-  monitoringRequestsBaits: any = [];
+  monitoringRequestsBaits: MonitoringRequestBait[] = [];
 
   constructor(
     private reportService: ReportsService
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
   }
 
   generateReport(form: CustomerFilter): void {
     this.isLoading = true;
-    this.reportService.getBaits(form).subscribe(() => {
+    this.reportService.getBaits(form).subscribe((monitoringRequestBaits) => {
+      this.monitoringRequestsBaits = monitoringRequestBaits;
       this.isLoading = false;
     });
   }
