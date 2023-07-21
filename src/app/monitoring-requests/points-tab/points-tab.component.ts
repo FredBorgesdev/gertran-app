62940649f76b414d1850cf6e3ba129e94efc0315
Vitor Freitas <vitorldfreitas@gmail.com>
2023-07-20 21:54:37 -0300

    add village as fallback for city

--- src/app/monitoring-requests/points-tab/points-tab.component.ts
+++ src/app/monitoring-requests/points-tab/points-tab.component.ts
@@ -189,7 +189,7 @@ export class PointsTabComponent implements OnInit {
   }
 
   async handleAddressChange(nominatimAddress: any, formGroup: FormGroup): Promise<void> {
-    const city = nominatimAddress.address.city || nominatimAddress.address.town;
+    const city = nominatimAddress.address.city || nominatimAddress.address.town || nominatimAddress.address.village;
     const state = brazilianStates.find(
       ({name}) => name === nominatimAddress.address.state
     ).abbreviation;
