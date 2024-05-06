75ed61572d7f3501061c7819f333040089cc6c61
Honório <hmneton@gmail.com>
2024-05-06 17:45:02 -0300

    revert function to print

--- src/app/monitoring-requests/monitoring-requests-check-list/monitoring-requests-check-list.component.ts
+++ src/app/monitoring-requests/monitoring-requests-check-list/monitoring-requests-check-list.component.ts
@@ -9,7 +9,6 @@ import MonitoringRequest from '../monitoring-request';
 import {ChecklistsService} from '../../checklists/checklists.service';
 import {AuthenticationService} from "../../authentication/authentication.service";
 import { ReportsService,BasePeriodFilter  } from 'src/app/reports/reports.service';
-import { NgxPrintElementService } from 'ngx-print-element';
 
 export type ModalDestroyResult = {
   updateList: boolean;
@@ -64,8 +63,7 @@ export class MonitoringRequestsCheckListComponent implements OnInit {
     private modal: NzModalRef,
     public checklistService: ChecklistsService,
     public authSevice: AuthenticationService,
-    public reportsService: ReportsService,
-    private printService: NgxPrintElementService
+    public reportsService: ReportsService
   ) {
   }
 
@@ -77,14 +75,6 @@ export class MonitoringRequestsCheckListComponent implements OnInit {
     this.loadChecklistBaitForm();
     this.loadMonitoringRequest();
     this.loadTerminals();
-  }
-  
-  onPrintClickCheckList(): void {
-    this.printService.print('checklist-print', this.printConfig);
-  }
-
-  onPrintClickReleasedVehicle(): void {
-    this.printService.print('release-print', this.printConfig);
   }
 
   loadChecklistForm(): void {
