54f13a6f8ab27bf6dadf35a8e6e8756994d2a6a2
hmneto <hmneton@gmail.com>
2024-08-30 10:40:53 -0300

    remove non_compliance_with_rules from incidents

--- src/app/incidents/create-incident-modal/create-incident-modal.component.ts
+++ src/app/incidents/create-incident-modal/create-incident-modal.component.ts
@@ -62,7 +62,7 @@ export class CreateIncidentModalComponent extends BaseCrudFormComponent<Incident
     {label: 'Evento teste', value: 'incident_test'},
     {label: 'Outro', value: 'others'},
     {label: 'Furto', value: 'theft'},
-    {label: 'Não condizente com as regras', value: 'non_compliance_with_rules'},
+    // {label: 'Não condizente com as regras', value: 'non_compliance_with_rules'},
     {label: 'Sem solicitação de monitoramento', value: 'without_monitoring_request'},
     {label: 'Parada prolongada', value: 'extended_stop'},
     {label: 'Acidente com terceiros', value: 'accident_with_third_party'},
