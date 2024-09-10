0e6a2eba83f5bff901be09b90339fe2f7834fdf5
hmneto <hmneton@gmail.com>
2024-09-10 17:57:29 -0300

    fix survey conducted by other + survey conducted by

--- src/app/shared/nominatim.service.ts
+++ src/app/shared/nominatim.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 
+// const BASE_URL = 'http://localhost:8033/v1/settings/points/get-lat-lng/?address=';
 const BASE_URL = 'https://api2.gertran.zayit.com.br/v1/settings/points/get-lat-lng/?address=';
 
 @Injectable({
