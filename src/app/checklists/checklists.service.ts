4b5676c52f854aa3a59be3103f293792382fce06
hmneto <hmneton@gmail.com>
2024-09-19 16:17:40 -0300

    add label related valid checklist date

--- src/app/checklists/checklists.service.ts
+++ src/app/checklists/checklists.service.ts
@@ -5,6 +5,7 @@ import {Observable} from 'rxjs';
 import User from "../users/user";
 
 export interface Checklist {
+  expirationDate: string;
   updatedAt: string;
   createdAt: string;
   status: string;
