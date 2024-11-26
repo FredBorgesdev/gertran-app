c7cd72029a0a23b4ae48ad1e2fc9ac3bccc3632b
hmneto <hmneton@gmail.com>
2024-11-26 09:40:32 -0300

    update mapbox

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'https://api.gertran.zayit.com.br:443/v1',
   sentryDsn: 'https://ad60a38f5d014bb091362c6415a6a6d1@o124348.ingest.sentry.io/6727350',
   appUrl: 'https://gertran.zayit.com.br',
-  mapboxAccessToken: 'pk.eyJ1Ijoidml0b3JsZGZyZWl0YXMiLCJhIjoiY2w4amppY25kMDQ4ODNucWc5Ynh6MTc4biJ9.9T9N2GtMEAwgo88NSwHayA'
+  mapboxAccessToken: 'pk.eyJ1IjoiZ2VydHJhbiIsImEiOiJjbTN5Zm90cHIwZmlnMmlxMWRiczhpcnhrIn0.MtKUH9jesayki1GEdIn5Og'
 };
