7e0629d249d94b40999679b5d87e3a331f9edf72
Honório <hmneton@gmail.com>
2025-02-28 10:07:54 -0300

    add risk area

---
+++ src/app/monitoring/risk-area.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import ApiService, { GetAllResponse, Pagination, DEFAULT_LIMIT } from '../shared/services/api.service';
+
+export interface RiskArea {
+  id: string;
+  name: string;
+  latitude: number;
+  longitude: number;
+  radius_risk_area: number;
+  customer: any;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RiskAreaService implements ApiService<RiskArea> {
+  private readonly endpoint = 'customers/risk-area';
+
+  constructor(private http: HttpClient) {}
+  get(id: string, ...params: any): Observable<RiskArea> {
+    throw new Error('Method not implemented.');
+  }
+  save(data: RiskArea, ...params: any): Observable<RiskArea> {
+    throw new Error('Method not implemented.');
+  }
+  update(id: string, data: RiskArea, ...params: any): Observable<RiskArea> {
+    throw new Error('Method not implemented.');
+  }
+  delete(id: string, ...params: any): Observable<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  getAll(pagination: Pagination): Observable<GetAllResponse<RiskArea>> {
+    const params: any = { limit: pagination.limit || DEFAULT_LIMIT };
+    
+    if (pagination.url) {
+      new URL(pagination.url).searchParams.forEach((value, key) => {
+        params[key] = value;
+      });
+    }
+
+    return this.http.get<GetAllResponse<RiskArea>>(this.endpoint, { params });
+  }
+}
