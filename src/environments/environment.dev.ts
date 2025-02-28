714b11d9081cc7cf9fe3306440e91af2aba5ab67
Honório <hmneton@gmail.com>
2025-02-28 10:08:54 -0300

    change api token mapbox

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://api.gertran.zayit.com.br/v1',
+  // apiUrl: 'https://api.gertran.zayit.com.br/v1',
   // apiUrl: 'https://0479-186-248-196-236.ngrok-free.app/v1',
-  // apiUrl: 'http://0.0.0.0:8033/v1',
+  apiUrl: 'http://0.0.0.0:8033/v1',
   sentryDsn: 'https://ad60a38f5d014bb091362c6415a6a6d1@o124348.ingest.sentry.io/6727350',
   appUrl: 'https://gertran.zayit.com.br',
-  mapboxAccessToken: 'pk.eyJ1IjoiZ2VydHJhbiIsImEiOiJjbTN5ZzYxNG4xb3lyMmlweGdyZGsyZHdyIn0.KeJXZxm1iv3GEZetvP8lDQ'
+  mapboxAccessToken: 'pk.eyJ1IjoiZ2VydHJhbjIiLCJhIjoiY203b3I0em1xMDNyYzJqb2NlbWl1cTk2ZCJ9.56ieJ5KKJI-TbOuGczP-3Q'
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'https://api.gertran.zayit.com.br:443/v1',
   sentryDsn: 'https://ad60a38f5d014bb091362c6415a6a6d1@o124348.ingest.sentry.io/6727350',
   appUrl: 'https://gertran.zayit.com.br',
-  mapboxAccessToken: 'pk.eyJ1IjoiZ2VydHJhbiIsImEiOiJjbTN5ZzYxNG4xb3lyMmlweGdyZGsyZHdyIn0.KeJXZxm1iv3GEZetvP8lDQ'
+  mapboxAccessToken: 'pk.eyJ1IjoiZ2VydHJhbjIiLCJhIjoiY203b3I0em1xMDNyYzJqb2NlbWl1cTk2ZCJ9.56ieJ5KKJI-TbOuGczP-3Q'
 };
